bf68c8755bc8c3b755b782e3525582d76c05e8bf
Michał <michal@mniewczas.pl>
2018-04-10 00:41:32 +0200

    bot high

--- bothigh.cpp
+++ bothigh.cpp
@@ -5,12 +5,20 @@ int bot_high(int points_bot1, int points_bot2, int round, int wybory1[], int wyb
     int enemyChoice = wybory2[round-1];
     int previousEnemyChoice = wybory2[round-2];
     int botChoice = wybory1[round-1];
+    int max = 0;
+    for(int i = round-1; i > 0; i--){
+        int tMax = wybory2[i] - wybory1[i-1];
+        if (tMax > max){
+            max = tMax;
+        }
+    }
+    int tact = enemyChoice-(max + 1);
     if(round == 0){
         return rand() % 120 + 180;
     }
-    if(botChoice > enemyChoice){
-        if(enemyChoice - 20 >= 180)
-            return enemyChoice - (rand() % 20);
+    if(botChoice >= enemyChoice){
+        if(tact >= 180)
+            return tact;
         return 300;
     }
     else{
