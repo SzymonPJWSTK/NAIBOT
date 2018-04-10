793773d6d04b06a46050b0d77d2729deeecc16ea
SzymonPJWSTK <s15754@pjwstk.edu.pl>
2018-04-10 12:47:16 +0200

    x_de

--- bothigh.cpp
+++ bothigh.cpp
@@ -1,38 +1,38 @@
 #include "bothigh.h"
 
-int bot_high(int points_bot1, int points_bot2, int round, int wybory1[], int wybory2[])
+int x_de(int points_bot1, int points_bot2, int round, int wybory1[], int wybory2[])
 {
-    if(round == 0){
-        return 220;
+ if(round == 0){
+        return 180;
     }
     int enemyChoice = wybory2[round-1];
     int previousEnemyChoice = wybory2[round-2];
     int botChoice = wybory1[round-1];
     int previousBotChoice = wybory1[round-2];
     int max = 0;
-    for(int i = round-1; i > 0; i--){
+    int limit = 0;
+    if(round < 10)
+        limit = round;
+    else
+        limit = 10;
+    for(int i = round-1; i > round - limit; i--){
         int tMax = previousBotChoice - enemyChoice;
-        if (tMax > max){
+        if (tMax > max && tMax != 120){
             max = tMax;
         }
     }
+
+    if(botChoice > 250)
+        return 200;
+
     if (max == 120)
         max--;
     int tact = enemyChoice - (max + 1);
-    if((botChoice == 300 && previousBotChoice != 300 )|| botChoice == 180)
+    if(enemyChoice == 180)
+        return 300 - max - 1;
+    if(botChoice == 180)
         return 300;
-    //if(botChoice >= enemyChoice){
-        if(tact >= 180)
-            return tact;
-        return 180;
-    //}
-    /*
-    else{
-        if(((enemyChoice - botChoice)/2) + botChoice > 300)
-            return 300;
-        return ((enemyChoice - botChoice)/2) + botChoice;
-    }
-    */
-    std::cout << "ups" << std::endl;
+    if(tact >= 180)
+        return tact;
     return 180;
 }
--- botlow.cpp
+++ botlow.cpp
@@ -1,48 +1,26 @@
 #include "botlow.h"
 
-int bot_low(int points_bot1, int points_bot2, int round, int wybory1[], int wybory2[])
+int x_de(int points_bot1, int points_bot2, int round, int wybory1[], int wybory2[])
 {
+   
     int enemyChoice = wybory2[round-1];
     int botChoice = wybory1[round-1];
-
-    int max = 0;
-    for(int i = round-1; i > 0; i--){
-        int tMax = wybory2[i] - wybory1[i-1];
-        if (tMax > max){
-            max = tMax;
-        }
-    }
-
     if(round == 0){
-        return 180;
-    }
-
-    if(botChoice == 280 || botChoice == 300)
         return 220;
-    else if (botChoice < 200)
+    }
+    if(botChoice > enemyChoice){
+        if(enemyChoice > 190)
+            return enemyChoice - 10;
+        if(enemyChoice > 185)
+            return enemyChoice - 5;
+        if(enemyChoice > 180)
+            return enemyChoice - 1;
         return 180;
-
-    int ranValue = (rand() % 20);
-
-    int tact = enemyChoice-(max + 1);
-    if(botChoice > enemyChoice)
-    {
-          if(tact >= 180)
-            return tact;
-        return 300;
     }
-    else if (botChoice == enemyChoice)
-    {
-        if(enemyChoice - ranValue >= 180)
-            return enemyChoice - ranValue;
-        else
-            return 280;
+    else{
+        if(((enemyChoice - botChoice)/2) + botChoice > 300)
+            return 300;
+        return ((enemyChoice - botChoice)/2) + botChoice;
     }
-    else
-    {
-        return 230;
-    }
-
-
     return 300;
 }
