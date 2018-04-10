38d287a5060e347c1d24ad642f8961c762c4b331
SzymonPJWSTK <s15754@pjwstk.edu.pl>
2018-04-10 10:08:07 +0200

    ih

--- botlow.cpp
+++ botlow.cpp
@@ -6,43 +6,29 @@ int bot_low(int points_bot1, int points_bot2, int round, int wybory1[], int wybo
     int botChoice = wybory1[round-1];
 
     int max = 0;
-    for(int i = round-1; i > 0; i--){
-        int tMax = wybory2[i] - wybory1[i-1];
+    for(int i = round-1; i > 1; i--){
+        int tMax = abs(wybory2[i] - wybory2[i-1]);
         if (tMax > max){
             max = tMax;
         }
     }
 
-    if(round == 0){
-        return 180;
-    }
+    
 
-    if(botChoice == 280 || botChoice == 300)
+     if(round == 0){
         return 220;
-    else if (botChoice < 200)
+    }
+    
+
+    int tact = enemyChoice-max - 1;
+
+    if(!(botChoice > enemyChoice)){
+        return rand() % 120 + 180;;
+    }
+    else{
+          if(tact > 180 && tact < 300)
+             return tact;
         return 180;
-
-    int ranValue = (rand() % 20);
-
-    int tact = enemyChoice-(max + 1);
-    if(botChoice > enemyChoice)
-    {
-          if(tact >= 180)
-            return tact;
-        return 300;
     }
-    else if (botChoice == enemyChoice)
-    {
-        if(enemyChoice - ranValue >= 180)
-            return enemyChoice - ranValue;
-        else
-            return 280;
-    }
-    else
-    {
-        return 230;
-    }
-
-
     return 300;
 }
